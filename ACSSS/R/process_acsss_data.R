58c5bf7ceef5f532344be14fd24e944f58a6e37f
murrayds <dakota.s.murray@gmail.com>
2019-08-13 10:46:41 -0400

    Refactored loader function, instead is now a simple processing function

--- ACSSS/R/process_acsss_data.R
+++ ACSSS/R/process_acsss_data.R
@@ -1,17 +1,16 @@
 #' The length of a string (in characters).
 #'
-#' @param path path to the ACSSS study data file
+#' @param unprocessed Dataframe containing the unprocessed data
 #' @export
-load_acsss_data <- function(path) {
-  dat <- read.csv(path, na.strings = '') %>%
+process_acsss_data <- function(unprocessed) {
+  processed <- unprocessed %>%
     dplyr::mutate(
-      Bio_sentiment = SentimentAnalysis::analyzeSentiment(as.character(Biography))$SentimentQDAP,
       Iteration = interaction(Year, Location),
       Country_University = as.character(Country_University),
       Country_University = ifelse(
         Country_University %in% c('US', 'USA', 'United States'), 'USA', Country_University
-      ),
+      )
     )
 
-  return(dat)
+  return(processed)
 }
