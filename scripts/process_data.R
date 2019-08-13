55656c021b10576e34fcc4aa9b281d1bb70f8b35
murrayds <dakota.s.murray@gmail.com>
2019-08-13 10:48:59 -0400

    Workflow script to run our processing function on the data and save

---
+++ scripts/process_data.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env RScript
+#
+# This is a simple script to process the ACSSS data and output the result
+
+# Parse command line arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+# the path to the data should be the first command line argument
+path_to_data <- args[1]
+
+# And output path is the second argument
+output_path <- args[2]
+
+# Read in the raw data
+raw_data <- read.csv(path_to_data, na.strings = '')
+
+# Perform the processing using the function in the ACSSS package
+processed <- ACSSS::process_acsss_data(raw_data)
+
+# And finally write the file
+write.csv(processed, file = output_path)
