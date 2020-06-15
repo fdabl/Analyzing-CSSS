b11c9e308ed0332733a4a3aa0d1050667a51b4de
Emily Coco <ec3307@nyu.edu>
2020-06-15 12:12:38 -0400

    eigencentrality null models by attribute, calculating and plotting

--- scripts/get_eigenvector.R
+++ scripts/get_eigenvector.R
@@ -8,6 +8,8 @@ library(ggplot2)
 library(lme4)
 library(MASS)
 library(dplyr)
+library(censReg)
+library(pscl)
 
 data <- read.csv("data/raw/cleaned_csss-all.csv")
 data <- data %>% filter(Year != 2011)
@@ -19,7 +21,8 @@ processed <- processed %>%
   filter(Location == "Santa Fe")
 
 ####Create Eigencentrality Dataframes####
-get_eigen_by_year <- function(df, iter) {
+
+get_eigen_by_year <- function(df, iter, attr = "discp") {
   edges <- build_edge_dataframe(df, iter)
   nodes <- build_node_dataframe(df, iter)
   
@@ -27,11 +30,11 @@ get_eigen_by_year <- function(df, iter) {
   nodes <- nodes %>%
     left_join(get_eigen_centrality(graph), by = "id")
   ec <- nodes %>%  
-    group_by(discp) %>%
+    group_by_at(attr) %>%
     summarize(mean.ec = mean(eigen_centrality), 
               max.ec = max(eigen_centrality), 
               min.ec = min(eigen_centrality)) %>%
-    arrange(discp) %>%
+    #arrange(discp) %>%
     mutate(iteration = iter)
   return(ec)
 }
@@ -45,11 +48,11 @@ get_prop_people_by_year <- function(df) {
     mutate(prop = n.ind/total)
 }
 
-get_all_eigencent <- function(df) {
+get_all_eigencent <- function(df, attr = "discp") {
   iters <- unique(df$Iteration)
-  all.ec <- get_eigen_by_year(df, iters[1])
+  all.ec <- get_eigen_by_year(df, iters[1], attr)
   for(i in 2:length(iters)) {
-    all.ec <- rbind(all.ec, get_eigen_by_year(df, iters[i]))
+    all.ec <- rbind(all.ec, get_eigen_by_year(df, iters[i], attr))
   }
   
   all.ec <- all.ec %>%
@@ -82,10 +85,15 @@ get_eigen_by_node = function(df) {
 
 node.ec <- get_eigen_by_node(processed)
 hist(node.ec$eigen_centrality)
+node.ec$pos.var = factor(node.ec$pos.var, levels = c("Student", "Postdoc", "Professor", "Researcher", "Government", "Industry", "Medicine", "Other"))
+node.ec$prstg = factor(node.ec$prstg, levels = c("Top 50", "51-100", "101-150", "151-200", "201-300", "301-400", "401-500", "501-600", "Unlisted", "Not University"))
+node.ec$gender = factor(node.ec$gender, levels = c("Male", "Female"))
+#node.ec$discp = factor(node.ec2$discp, levels = c("Physical sciences"))
+node.ec[node.ec == ""] = NA
 node.ec2 = node.ec %>% filter(eigen_centrality != 0)
 hist(node.ec2$eigen_centrality)
 
-fit1 = lm(eigen_centrality ~ as.factor(discp) + as.factor(pos.var) + as.factor(prstg) + as.factor(gender),
+fit1 = glm(eigen_centrality ~ as.factor(discp) + as.factor(pos.var) + as.factor(prstg) + as.factor(gender),
           data = node.ec, na.action = na.omit)
 plot(fit1, which = 2)
 plot(fit1, which = 1)
@@ -100,13 +108,16 @@ lambda = bc$x[which.max(bc$y)]
 node.ec2$ec.transformed = (((node.ec2$eigen_centrality)^lambda)-1)/lambda
 hist(node.ec2$ec.transformed)
 node.ec2[node.ec2 == ""] = NA
+node.ec2$pos.var = factor(node.ec2$pos.var, levels = c("Student", "Postdoc", "Professor", "Researcher", "Government", "Industry", "Medicine", "Other"))
+node.ec2$prstg = factor(node.ec2$prstg, levels = c("Top 50", "51-100", "101-150", "151-200", "201-300", "301-400", "401-500", "501-600", "Unlisted", "Not University"))
+node.ec2$gender = factor(node.ec2$gender, levels = c("Male", "Female"))
+#node.ec2$discp = factor(node.ec2$discp, levels = c("Physical sciences"))
 
 fit2 = lm(ec.transformed ~ as.factor(discp) + as.factor(pos.var) + as.factor(prstg) + as.factor(gender),
           data = node.ec2, na.action = na.omit)
 plot(fit2, which = 2)
 plot(fit2, which = 1)
 summary(fit2)
-levels(as.factor(node.ec2$pos.var))
 
 
 ####Plotting Functions####
@@ -169,14 +180,104 @@ plot_all_eigen <- function(df) {
   )
 }
 
-all.ec <- get_all_eigencent(processed)
-prop.discp <- get_prop_people_by_year(processed) %>%
-  mutate(discp = discp1, year = Year)
+all.ec <- get_all_eigencent(processed, attr = "discp")
+# prop.discp <- get_prop_people_by_year(processed) %>%
+#   mutate(discp = discp1, year = Year)
+# 
+# ggsave("figures/eigencentrality.png", plot_eigen_lines(all.ec), 
+#        width = 5, height = 2.5, scale = 1.75)
+# ggsave("figures/eigen_prop-of-people.png", plot_eigen_and_prop(all.ec, prop.discp), 
+#        width = 6.5, height = 4, scale = 1.75)
+# ggsave("figures/all-discp_eigen-values.png", plot_all_eigen(all.ec), 
+#        width = 8, height = 5.5, scale = 1.25)
 
-ggsave("figures/eigencentrality.png", plot_eigen_lines(all.ec), 
-       width = 5, height = 2.5, scale = 1.75)
-ggsave("figures/eigen_prop-of-people.png", plot_eigen_and_prop(all.ec, prop.discp), 
-       width = 6.5, height = 4, scale = 1.75)
-ggsave("figures/all-discp_eigen-values.png", plot_all_eigen(all.ec), 
+####Null Models####
+randomize_node_values = function(processed, iter, attr = "discp"){
+  nodes <- build_node_dataframe(processed, iter)
+  shuffled = nodes %>% dplyr::select_at(attr)
+  rows = sample(nrow(shuffled))
+  nodes[attr] = shuffled[rows,]
+  return(nodes)
+}
+
+generate_null_ec_data = function(processed, attr = "discp", n) {
+  iters = unique(processed$Iteration)
+  edges <- build_edge_dataframe(processed, iters[1])
+  nodes = randomize_node_values(processed, iters[1], attr)
+  graph = create_graph(nodes, edges)
+  nodes = nodes %>%
+    left_join(get_eigen_centrality(graph), by = "id")
+  ec = nodes %>%  
+    group_by_at(attr) %>%
+    summarize(mean.ec = mean(eigen_centrality), 
+              max.ec = max(eigen_centrality), 
+              min.ec = min(eigen_centrality)) %>%
+    #arrange(attr) %>%
+    mutate(iteration = iters[1])
+  
+  for(i in 2:length(iters)) {
+    print(i)
+    for(j in 1:n) {
+      ##shuffle attributes
+      edges <- build_edge_dataframe(processed, iters[i])
+      nodes = randomize_node_values(processed, iters[i], attr)
+      
+      graph = create_graph(nodes, edges)
+      nodes = nodes %>%
+        left_join(get_eigen_centrality(graph), by = "id")
+      ec1 = nodes %>%  
+        group_by_at(attr) %>%
+        summarize(mean.ec = mean(eigen_centrality), 
+                  max.ec = max(eigen_centrality), 
+                  min.ec = min(eigen_centrality)) %>%
+        #arrange(attr) %>%
+        mutate(iteration = iters[i])
+      ec = rbind(ec, ec1)
+    }
+  }
+  return(ec)
+}
+
+#generate plot for discipline eigencentrality
+plot_ec_null_models = function(processed, attr = "discp") {
+  all.ec = get_all_eigencent(processed, attr)
+  all.ec[all.ec == ""] = NA
+  nulldf = generate_null_ec_data(processed, attr, 50) %>%
+    mutate(iteration = str_replace(iteration, "[.]", ",")) %>%
+    separate(iteration, c("year", "location"), sep = ",") %>%
+    filter(location == "Santa Fe", year != "2005")
+  grouping = c(attr, "year")
+  ci = nulldf %>% group_by_at(grouping) %>%
+    summarize(upper_ci = quantile(mean.ec, 0.975), 
+              lower_ci = quantile(mean.ec, 0.025))
+  ci[ci == ""] = NA
+  p = ggplot() +
+    geom_errorbar(data = na.omit(ci), aes(x = year, group = year, ymin = lower_ci, ymax = upper_ci), color = "grey") +
+    geom_point(data = na.omit(all.ec), aes(x = year, group = year, y = mean.ec), color = "black", size = 2) +
+    facet_wrap(as.character(attr)) +
+    theme_minimal() +
+    labs(x = "Year", y = "Eigencentrality") +
+    theme(axis.text.x = element_text(angle = 90), panel.spacing = unit(2, "lines"))
+  return(p)
+}
+
+dplot = plot_ec_null_models(processed, attr = "discp")
+plot(dplot)
+ggsave("figures/eigencentrality_null_discp.png", dplot,
        width = 8, height = 5.5, scale = 1.25)
+
+gplot = plot_ec_null_models(processed, attr = "gender")
+plot(gplot)
+ggsave("figures/eigencentrality_null_gender.png", gplot, 
+       width = 5, height = 2.5, scale = 1.25)
+
+pplot = plot_ec_null_models(processed, attr = "pos.var")
+plot(pplot)
+ggsave("figures/eigencentrality_null_position.png", pplot,
+       width = 6.5, height = 4, scale = 1.25)
+
+prplot = plot_ec_null_models(processed, attr = "prstg")
+plot(prplot)
+ggsave("figures/eigencentrality_null_prestige.png", prplot,
+       width = 6.5, height = 4, scale = 1.25)
 
